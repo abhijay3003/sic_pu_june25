d1271207a2b50d639d45bc949044e443f94aa9eb
abhijay3003 <abhijay9505@gmail.com>
2025-06-25 16:55:13 +0530

    Fix: Added taxable income calculation

--- week1_assignments/taxation.py
+++ week1_assignments/taxation.py
@@ -1,5 +1,12 @@
 Employee_name = input("Enter the  employee Name : ")
 Employee_id = int(input("Enter the employee id :"))
 Basic_Monthly_salary = int(input(" Enter the employee basic monthly salary: "))
-Special_Allowances = int(input(" Enter the account of Special Allowances"))
-Bouns_percentage = int(input("Enter the bouns withou % symbol"))
+Special_Allowances = int(input(" Enter the account of Special Allowances : "))
+Bouns_percentage = int(input("Enter the bouns withou % symbol : "))
+Bouns_amount = (Bouns_percentage/100) * Basic_Monthly_salary
+Gross_monthly_salary = Basic_Monthly_salary + Special_Allowances
+Annual_gross_salary = (Gross_monthly_salary * 12) +Bouns_amount
+print("employee name: {}\nemployee id: {}\ngross monthly salary: {}\nannual gross salary: {}".format(Employee_name, Employee_id, Gross_monthly_salary, Annual_gross_salary))
+Standard_deduction = 50000
+Taxable_income = Annual_gross_salary - Standard_deduction
+print('Standard deduction : {}\ntaxable income : {}'.format(Standard_deduction,Taxable_income))
