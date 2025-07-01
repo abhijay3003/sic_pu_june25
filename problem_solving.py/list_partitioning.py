10204df24dafaecb64e7488f408daade12470d65
abhijay3003 <abhijay9505@gmail.com>
2025-07-01 11:19:42 +0530

    modified

--- problem_solving.py/list_partitioning.py
+++ problem_solving.py/list_partitioning.py
@@ -1,9 +1,14 @@
 n, x, y = map(int, input("Enter the numbers n, x, y separated by spaces: ").split())
-numbers = list(map(int, input(f"Enter {n} numbers separated by spaces: ").split()))
+numbers = []
+
+for i in range(n):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+
 numbers.sort()
 
 if 0 < y < n:
     p = numbers[y] - numbers[y - 1] - 1
-    print(p)
+    print("Result:", p)
 else:
     print("Invalid value for y. It must be between 1 and", n - 1)
